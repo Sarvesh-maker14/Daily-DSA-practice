a093adbb894af775e5ab6ea2c53865c87a6c88ef
Sarvesh <sarveshsingh1409@gmail.com>
2023-05-14 00:53:39 +0530

    Add existing project files to Git

---
+++ week6/leetcode/861.cpp
@@ -0,0 +1,30 @@
+//Score after flipping matrix -- 861
+
+
+
+
+
+
+
+// no of 1's jada != decimal value jada
+//110000 > 101111
+//48>47
+
+
+//left me jitne 1's jada hoge utni values jada hogi
+
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
